cde9bdd8d9b928d6eb863eee070ddfdd45c0b30b
Brandon Bledsoe <brbledsoe.m@gmail.com>
2018-04-29 09:53:44 +0300

    added closure to code

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-// an array with all of our cart items
-var cart = [];
+var ShoppingCart = function () {
 
-var updateCart = function () {
-  // TODO: Write this function. In this function we render the page.
-  // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
-}
+  // an array with all of our cart items
+  var cart = [];
+
+  var updateCart = function () {
+    // TODO: Write this function. In this function we render the page.
+    // Meaning we make sure that all our cart items are displayed in the browser.
+    // Remember to empty the "cart div" before you re-add all the item elements.
+  }
 
 
-var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-}
+  var addItem = function (item) {
+    // TODO: Write this function. Remember this function has nothing to do with display. 
+    // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+  }
 
-var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
-}
+  var clearCart = function () {
+    // TODO: Write a function that clears the cart ;-)
+  }
+  
+  return {
+    updateCart: updateCart,
+    addItem: addItem,
+    clearCart: clearCart
+  }
+};
+
+var app = ShoppingCart();
+
+// update the cart as soon as the page loads!
+app.updateCart();
+
+
+//--------EVENTS---------
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
@@ -23,13 +40,10 @@ $('.view-cart').on('click', function () {
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
-  addItem(item);
-  updateCart();
+  app.addItem(item);
+  app.updateCart();
 });
 
 $('.clear-cart').on('click', function () {
-  clearCart();
+  app.clearCart();
 });
-
-// update the cart as soon as the page loads!
-updateCart();
